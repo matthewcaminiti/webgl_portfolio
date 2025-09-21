d1c3424182a01123ff67b74fbd38a1b79c5f904f
matthewcaminiti <matthew.z.caminiti@gmail.com>
2025-09-21 17:27:03 -0600

    improve deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: "./src/index.ts",
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	} ,
-	resolve: {
-		extensions: [".ts", ".js"],
-	},
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: "bundle.js",
-	},
-}
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'assets', to: './assets' }],
+    }),
+  ],
+};
